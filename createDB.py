216261f082f465dcb1a95a6aea8d92b0b0d4276e
McRx71 <50086322+McRx71@users.noreply.github.com>
2021-03-28 12:48:34 +0200

    createDB better call
    if __name__ == '__main__':
        checkDB()
    
    For better importing

--- createDB.py
+++ createDB.py
@@ -1,22 +1,24 @@
 import sqlite3, os, sys
 
 
-class createDB:
+def checkDB():
+    # Existenz feststellen, wenn nicht vorhanden, dann v
+    if os.path.exists("timetable.db"):
+        pass
+    else:
+        # Datenbanktabelle erzeugen
+        connection = sqlite3.connect("timetable.db")
+        cursor = connection.cursor()
+        sql = "CREATE TABLE Chris(" \
+              "weeknumber TEXT, " \
+              "dayofweek TEXT, " \
+              "date DATE, " \
+              "time REAL, " \
+              "task TEXT," \
+              "FirstLevelSupport BOOLEAN)"
 
-    def checkDB():
-        # Existenz feststellen, wenn nicht vorhanden, dann v
-        if os.path.exists("timetable.db"):
-            pass
-        else:
-            # Datenbanktabelle erzeugen
-            connection = sqlite3.connect("timetable.db")
-            cursor = connection.cursor()
-            sql = "CREATE TABLE Chris(" \
-                  "weeknumber TEXT, " \
-                  "dayofweek TEXT, " \
-                  "date DATE, " \
-                  "time REAL, " \
-                  "task TEXT," \
-                  "FirstLevelSupport BOOLEAN)"
+        cursor.execute(sql)
 
-            cursor.execute(sql)
+
+if __name__ == '__main__':
+    checkDB()
